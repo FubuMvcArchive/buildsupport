874883ea35fb18f062626b35a2530ca89cdd0069
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-06-07 16:39:00 -0500

    The nuget rake commands now accept a package name as an argument.
    For example, to push all nuget packages to c:\nugs:
    rake nuget:push
    
    To push just the MyLibrary package to c:\nugs:
    rake nuget:push[mylibrary]

--- nuget.rb
+++ nuget.rb
@@ -11,8 +11,9 @@ namespace :nuget do
 	end
 	
 	desc "update dependencies from local machine"
-    task :pull do
+    task :pull, [:package] do |task, args|
     FileList[File.join(package_root, "*")].exclude{|f| File.file?(f)}.each do |package|
+      next if args[:package] && package_name(package).downcase != args[:package].downcase
       dst = File.join package, "lib"
       src = File.join @nugroot, package_name(package), "lib"
       if File.directory? src
@@ -32,8 +33,9 @@ namespace :nuget do
 	end
 
 	desc "pushes dependencies to central location on local machine for nuget:pull from other repos"
-  task :push do
+  task :push, [:package] => :build do |task, args|
     FileList["#{ARTIFACTS}/*.nupkg"].exclude(".symbols.nupkg").each do |file|
+      next if args[:package] && package_name(file).downcase != args[:package].downcase
       destination = File.join @nugroot, package_name(file)
       clean_dir destination
       unzip_file file, destination
@@ -72,19 +74,21 @@ namespace :nuget do
     }
   end
 	
-	desc "Pushes nuget packages to the official feed"
-	task :release do
+  desc "Pushes nuget packages to the official feed"
+  task :release, [:package] do |t, args|
     require 'open-uri'
 
     clean_dir 'packaging/release'
 
     artifact_url = "http://teamcity.codebetter.com/guestAuth/repository/downloadAll/#{@teamcity_build_id}/.lastSuccessful/artifacts.zip"
+    puts "downloading artifacts from teamcity.codebetter.com"
     artifact = open(artifact_url)
     unzip_file artifact.path, "packaging/release"
     FileList['packaging/release/*.nupkg'].exclude(".symbols.nupkg").each do |nupkg|
+      next if args[:package] && package_name(nupkg).downcase != args[:package].downcase
       sh "#{@nuget} push #{nupkg}" do |ok, res|
         puts "May not have published #{nupkg}" unless ok
       end
     end
-	end	
+  end	
 end

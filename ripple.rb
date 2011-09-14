dfa6e1c704874470a9d5265b69ef26ea88d2c393
Jeremy Miller <jeremydmiller@yahoo.com>
2011-09-13 21:16:53 -0500

    adding the right tasks

--- ripple.rb
+++ ripple.rb
@@ -1,3 +1,13 @@
-task :restore do
-  sh 'buildsupport/ripple.exe restore'
-end
+
+	desc "Restores nuget package files"
+	task :restore do
+	  puts 'Restoring all the nuget package files'
+	  sh 'buildsupport/ripple.exe restore'
+	end
+	
+	desc "Updates nuget package files to the latest"
+	task :update do
+	  puts 'Updating all the nuget package files'
+	  sh 'buildsupport/ripple.exe update'
+	end
+
